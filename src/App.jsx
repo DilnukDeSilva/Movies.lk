388851543097ae7df399b94f36b8202547259745
DilnukDeSilva <dilnukdesilva@gmail.com>
2025-09-01 14:55:40 +0530

    add movies and favorites

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Movies from "./pages/Movies";
 import MovieDetails from "./pages/MovieDetails";
 import SeatLayout from "./pages/SeatLayout";
 import MyBookings from "./pages/MyBookings";
-import Favorite from "./pages/Favorite";
+import Favorites from "./pages/Favorite";
 import {Toaster} from 'react-hot-toast'
 import Footer from "./components/Footer";
 
@@ -25,7 +25,7 @@ const App = () => {
           <Route path='/movies/:id' element={<MovieDetails/>}/>
           <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
           <Route path='/my-bookings' element={<MyBookings/>}/>
-          <Route path='/favorite' element={<Favorite/>}/>
+          <Route path='/favorites' element={<Favorites/>}/>
           
         </Routes>
         {!isAdminRoute && <Footer/>}
