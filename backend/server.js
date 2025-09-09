cf654755da5067c4f6178295ed862b730375e7f1
DilnukDeSilva <dilnukdesilva@gmail.com>
2025-09-09 09:56:10 +0530

    add inngest and clerk for user data

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors'
 import 'dotenv/config';
 import { connect } from 'mongoose';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,11 +16,11 @@ await connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Movies API Server Running');
-});
+app.get('/', (req, res) => res.send('Movies API Server Running'));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Start server
 app.listen(PORT, () => {
