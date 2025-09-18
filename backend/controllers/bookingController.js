c169e0ebfce57bcc039aa3ba13425eed704a64bd
DilnukDeSilva <dilnukdesilva@gmail.com>
2025-09-18 09:06:30 +0530

    add and complete function to release seats if not payed

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,3 +1,4 @@
+import { inngest } from "../inngest/index.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
 import stripe from 'stripe';
@@ -82,6 +83,15 @@ export const createBooking = async (req, res) => {
 
     booking.paymentLink = session.url
     await booking.save()
+
+    // Run Inngest sheduler function to check payment after 10 min realese seat
+    await inngest.send({
+      name: "app/checkpayment",
+      data: {
+          bookingId: booking._id.toString()
+      }
+    })
+
 
     res.json({ success: true, url: session.url});
 
