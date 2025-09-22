3bf60ebb522b1a9923ac7cce2eb028d2d3aeb491
DilnukDeSilva <dilnukdesilva@gmail.com>
2025-09-22 20:44:12 +0530

    fix sender email issue

--- backend/configs/nodeMailer.js
+++ backend/configs/nodeMailer.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
 
 const sendMail = async ({ to, subject, body }) => {  
     const response = await transporter.sendMail({  
-        from: process.env.SENDER_EMAIL,  
+        from: process.env.SENDER_MAIL,  
         to,  
         subject,  
         html: body,  
